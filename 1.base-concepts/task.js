5d758926fa5ea70cfe0c41b7ab140ca830271396
Anastasiia <ansol8@yandex.ru>
2026-02-23 23:12:36 +0300

    Оба задания выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b ** 2 - 4 * a * c;
+
+  if (d === 0) {
+    const x1 = -b / (2 * a);
+    arr.push(x1);
+  } else {
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push (x1, x2)
+  }
+
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+  let monthlyPercent = (percent / 100) / 12;
+
+  let creditBody = amount - contribution;
+
+  let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) -1)))
+
+  let totalAmount = Math.round(totalAmount * 100) /100;
+
+  return totalAmount;
   
 }

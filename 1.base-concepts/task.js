ba4a1a823ef8e2c6c3b45e70fed9a49741e0285a
Anastasiia <ansol8@yandex.ru>
2026-03-01 21:22:59 +0300

    Исправление ошибок

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,7 +3,9 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
 
-  if (d === 0) {
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
     const x1 = -b / (2 * a);
     arr.push(x1);
   } else {

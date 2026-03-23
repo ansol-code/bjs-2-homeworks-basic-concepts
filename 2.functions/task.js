65271e556faf1f1f2dccdbe339297d9b3a2d33ad
Anastasiia <ansol8@yandex.ru>
2026-03-23 23:13:49 +0300

    Функции. Первое задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,10 +1,53 @@
 function getArrayParams(...arr) {
 
+  if (arr.lenght === 0)
+
   return { min: min, max: max, avg: avg };
+
+
+let min = arr[0];
+let max = arr[0];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+
+  if (element > max) {
+    max = element;
+  }
+
+  if (element < min) {
+    min = element;
+  }
+
+  sum += element;
 }
 
-function summElementsWorker(...arr) {
+const avg = sum / arr.length;
 
+const roundedAvg = Number(avg.toFixed(2));
+
+const getArrayParams1 = [-99, 99, 10];
+const getArrayParams2 = [1, 2, 3, -100, 10];
+const getArrayParams3 = [5];
+
+console.log(getArrayParams1);
+console.log(getArrayParams2);
+console.log(getArrayParams3);
+
+return {
+  min: min,
+  max: max,
+  avg: roundedAvg
+};
+}
+
+
+
+
+
+function summElementsWorker(...arr) {
+ 
 }
 
 function differenceMaxMinWorker(...arr) {

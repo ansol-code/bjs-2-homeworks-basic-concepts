924d4780442dd31f3bb8e9a7d0ce57703512c60e
Anastasiia <ansol8@yandex.ru>
2026-04-07 20:08:34 +0300

    Второе задание

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,7 +1,7 @@
 class PrintEditionItem {
     constructor(name, releaseDate, pagesCount){
         this.name = name;
-        this.releaseDate =releaseDate;
+        this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
         this._state = 100;
         this.type = null;
@@ -23,6 +23,16 @@ set state(value) {
 }
  fix() {
     this.state = this._state * 1.5;
+ }
+
+ damage(percent) {
+    this.state = this.state - percent;
+    console.log("Книга повреждена на " + percent + "%. Состояние: " + this.state + "%");
+ }
+
+ restore(percent) {
+   this.state = this.state + percent;
+    console.log("Книга восстановлена на " + percent + "%. Состояние: " + this.state + "%"); 
  }
 }
 
@@ -81,3 +91,105 @@ class Book extends PrintEditionItem {
      console.log(picknick.state);
      picknick.fix();
      console.log(picknick.state);
+
+     //Задача 2
+     class Library {
+        constructor(name) {
+            this.name = name;
+            this.books = [];
+        }
+
+        addBook(book) {
+            if (book.state > 30) {
+                this.books.push(book);
+                console.log("Книга " + book.name + " добавлена в библиотеку");
+                return true;
+            } else {
+                console.log("Книга " + book.name + " сильно повреждена (состояние " + book.state + "%)");
+                return false;
+            }
+        }
+
+     findBookBy(type, value) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i][type] === value)
+                return this.books[i];
+        }
+        return null;
+     }
+
+     giveBookByName(bookName) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) {
+                const book = this.books.splice(i, 1)[0];
+                console.log("Книга " + book.name + " выдана");
+                return book;
+            }
+        }
+      console.log("Книга " + bookName + " не найдена");
+      return null;  
+     }
+    }
+
+    console.log("Тест");
+    const library = new Library("Библиотека имени Ленина");
+    console.log("Создана библиотека: " + library.name)
+
+    library.addBook(
+        new DetectiveBook(
+        "Артур Конан Дойл",
+        "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+        2019,
+        1008
+    )
+);
+library.addBook(
+    new FantasticBook(
+        "Аркадий и Борис Стругацкие",
+        "Пикник на обочине",
+        1972,
+        168
+    )
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+library.addBook(new FantasticBook("Абрахам Меррит", "Лунная заводь", 1919, 433));
+
+console.log(library.findBookBy("name", "Властелин колец"));
+console.log(library.findBookBy("releaseDate", 1924).name);
+console.log(library.findBookBy("releaseDate", 1919).name);
+
+console.log("Выдача книги");
+const givenBook = library.giveBookByName("Машина времени");
+
+console.log("Повреждение книги");
+if (givenBook) {
+    givenBook.damage(40);
+    console.log("Книга повреждена. Состояние: " + givenBook.state + "%");
+}
+
+console.log("Восстановление книги");
+if (givenBook) {
+    givenBook.restore(70);
+    console.log("Книга восстановлена. Состояние: " + givenBook.state + "%");
+}
+
+console.log("Добавление восстановленной книги в библиотеку");
+if (givenBook) {
+    const added = library.addBook(givenBook);
+    if (added) {
+        console.log("Книга возвращена в библиотеку.");
+    } else {
+        console.log("Книгу нельзя вернуть в библиотеку, так как её состояние " + givenBook.state + "%");
+    }
+}
+
+
+console.log("Количество книг до выдачи: " + library.books.length);
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length);
+
+
+
+
+
